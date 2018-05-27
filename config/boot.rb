f4e660d90eee009c1da148cae5828e14e07293ac
John Romkey <romkey@romkey.com>
2018-05-26 21:25:11 -0700

    do not bootsnap on a Pi

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,7 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+
+# this blows up on a Raspberry Pi
+
+#require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
