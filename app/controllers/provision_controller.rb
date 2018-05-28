b2c73db66fd71e5e5fa6ac9de861f6bc4ffd4e80
John Romkey <romkey@romkey.com>
2018-05-28 15:24:24 -0700

    get the default MQTT port correct

--- app/controllers/provision_controller.rb
+++ app/controllers/provision_controller.rb
@@ -27,7 +27,7 @@ class ProvisionController < ApplicationController
         response = { uuid: pr.id,
                      status: 'provisioned',
                      mqtt_hostname: 'homebus',
-                     mqtt_port: 80,
+                     mqtt_port: 1883,
                      mqtt_username: pr.mosquitto_account.id,
                      mqtt_password: password
                    }
