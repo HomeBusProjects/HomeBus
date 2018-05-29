a2d00ef5d13a5a43632f53d0821d5fc10374e0e7
John Romkey <romkey@romkey.com>
2018-05-29 08:39:43 -0700

    git rid of usernames from mosquitto accounts (:id is the username)

--- app/controllers/mosquitto_accounts_controller.rb
+++ app/controllers/mosquitto_accounts_controller.rb
@@ -69,6 +69,6 @@ class MosquittoAccountsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def mosquitto_account_params
-      params.require(:mosquitto_account).permit(:username, :password, :superuser, :provision_request_id)
+      params.require(:mosquitto_account).permit(:password, :superuser, :provision_request_id)
     end
 end
