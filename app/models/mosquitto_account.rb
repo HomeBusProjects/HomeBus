cef64800950e0387d79630714a8b35f89cc2aaa4
John Romkey <romkey@romkey.com>
2018-05-28 20:29:15 -0700

    fixed password generation bug

--- app/models/mosquitto_account.rb
+++ app/models/mosquitto_account.rb
@@ -4,10 +4,11 @@ class MosquittoAccount < ApplicationRecord
   def generate_password!
     unencoded_password = _get_random_string(32)
 
-    hashed_password = `np -p #{password}`
+    hashed_password = `np -i 1000 -p #{unencoded_password}`
     hashed_password.chomp!
 
     puts "unencoded password for device is #{unencoded_password}"
+    puts "encoded password for device is #{hashed_password}"
     self.password = hashed_password
     self.save
 
