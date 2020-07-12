19f6f62b0f24ea5054bcaaed706b012da295e25d
John Romkey <58883+romkey@users.noreply.github.com>
2020-07-11 17:03:43 -0700

    correctly remove devices when deleting provision_request

--- app/controllers/provision_requests_controller.rb
+++ app/controllers/provision_requests_controller.rb
@@ -117,7 +117,7 @@ class ProvisionRequestsController < ApplicationController
   def destroy
     @provision_request.mosquitto_account.delete
     @provision_request.mosquitto_acl.delete_all
-    @provision_request.device.delete_all
+    @provision_request.devices.delete_all
 
     @provision_request.destroy
     respond_to do |format|
