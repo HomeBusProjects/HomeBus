78e134841ee496ccd677b1090d52f3d0e0a4863e
John Romkey <58883+romkey@users.noreply.github.com>
2020-08-20 10:48:00 -0700

    added UUIDs to show
    start generating ACLs

--- app/models/provision_request.rb
+++ app/models/provision_request.rb
@@ -3,4 +3,10 @@ class ProvisionRequest < ApplicationRecord
   has_many :devices
   has_one :mosquitto_account
   has_many :mosquitto_acl
+
+  def _generate_acls
+    wo_ddcs.flat_map do |ddc|
+      'homebus/device/' + id + '/' + ddc
+    end
+  end
 end
