0c98f8261bc8b58eb850cd900d994568b2acde89
John Romkey <58883+romkey@users.noreply.github.com>
2020-10-08 15:21:54 -0700

    attempt at mosquitto ACL generation

--- app/models/mosquitto_acl.rb
+++ app/models/mosquitto_acl.rb
@@ -4,4 +4,27 @@ class MosquittoAcl < MosquittoRecord
   belongs_to :provision_request
 
   bitfield :permissions, 1 => :read, 2 => :write, 4 => :subscribe
+
+  def self.from_provision_request(pr)
+    account = MosquittoAccount.find_by provision_request_id: pr.id
+
+    ActiveRecord::Base.transaction do
+      pr.mosquitto_acls.delete_all
+      pr.devices.each do |device|
+        device.wo_ddcs.each do |ddc|
+          MosquittoAcl.create username: account.username,
+                              topic: "homebus/device/#{device.id}/#{ddc}",
+                              permissions: :read,
+                              provision_request_id: pr.id
+        end
+
+        device.ro_ddcs.each do |ddc|
+          MosquittoAcl.create username: account.username,
+                              topic: "homebus/device/+/#{ddc}",
+                              permissions: :write,
+                              provision_request_id: pr.id
+        end
+      end
+    end
+  end
 end
