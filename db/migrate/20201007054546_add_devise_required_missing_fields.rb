673cebdfe70a74af4aaf2309c52f6f86608c4785
John Romkey <58883+romkey@users.noreply.github.com>
2020-10-07 17:27:38 -0700

    rewrote migration

--- db/migrate/20201007054546_add_devise_required_missing_fields.rb
+++ db/migrate/20201007054546_add_devise_required_missing_fields.rb
@@ -1,28 +1,20 @@
 class AddDeviseRequiredMissingFields < ActiveRecord::Migration[6.0]
-  def up
-    change_table(:users) do |t|
-      ## Confirmable
-      t.string   :confirmation_token, unique: true
-      t.datetime :confirmed_at
-      t.datetime :confirmation_sent_at
-      t.string   :unconfirmed_email # Only if using reconfirmable
-
-      User.update_all confirmed_at: DateTime.now
-
-      ## Lockable
-      t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      t.string   :unlock_token # Only if unlock strategy is :email or :both
-      t.datetime :locked_at
-    end 
+  def change
+    ## Confirmable
+    add_column :users, :confirmation_token, unique: true
+    add_column :users, :confirmed_at
+    add_column :users, :confirmation_sent_at
+    add_column :users, :unconfirmed_email # Only if using reconfirmable
 
     add_index :users, :confirmation_token, unique: true
+
+    User.update_all confirmed_at: DateTime.now
+
+    # lockable
+    add_column :users, :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
+    add_column :users, :unlock_token # Only if unlock strategy is :email or :both
+    add_column :users, :locked_at
+
     add_index :users, :unlock_token, unique: true
-  end
-
-  def down
-    remove_index :users, :confirmation_token
-    remove_index :users, :unlock_token
-
-    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :failed_attempts, :unlock_token, :locked_at
   end
 end
