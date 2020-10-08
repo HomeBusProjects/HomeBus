bc89dd3886072540b17d3934b8aee9d7cb008389
John Romkey <58883+romkey@users.noreply.github.com>
2020-10-07 17:39:45 -0700

    fix the migration again

--- db/migrate/20201007054546_add_devise_required_missing_fields.rb
+++ db/migrate/20201007054546_add_devise_required_missing_fields.rb
@@ -1,19 +1,19 @@
 class AddDeviseRequiredMissingFields < ActiveRecord::Migration[6.0]
   def change
     ## Confirmable
-    add_column :users, :confirmation_token
-    add_column :users, :confirmed_at
-    add_column :users, :confirmation_sent_at
-    add_column :users, :unconfirmed_email # Only if using reconfirmable
+    add_column :users, :confirmation_token, :string
+    add_column :users, :confirmed_at, :datetime
+    add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string # Only if using reconfirmable
 
     add_index :users, :confirmation_token, unique: true
 
     User.update_all confirmed_at: DateTime.now
 
     # lockable
-    add_column :users, :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-    add_column :users, :unlock_token # Only if unlock strategy is :email or :both
-    add_column :users, :locked_at
+    add_column :users, :failed_attempts, :integer, default: 0, null: false # Only if lock strategy is :failed_attempts
+    add_column :users, :unlock_token, :string # Only if unlock strategy is :email or :both
+    add_column :users, :locked_at, :datetime
 
     add_index :users, :unlock_token, unique: true
   end
