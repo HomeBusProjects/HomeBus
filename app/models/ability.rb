e79c237d0f04a99e3a7386a0a91fdf15f9a3d6f2
John Romkey <58883+romkey@users.noreply.github.com>
2020-10-10 15:56:46 -0700

    new try at writing the authorization check for managing requests

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,7 +11,11 @@ class Ability
       can :manage, User, id: user.id # permissions for every user, even if not logged in    
       can :manage, Network, id: user.networks.pluck(:id)
       can [ :create, :new], Network
-#      can :manage, ProvisionRequest, id: ProvisionRequest.owned_by(user)
+      #      can :manage, ProvisionRequest, id: ProvisionRequest.owned_by(user)
+      can :manage, ProvisionRequest do |pr|
+        user.networks.pluck(:id).include?(pr.network_id)
+      end
+
       can :manage, Device, id: user.devices.pluck(:id)
       can :read, Ddc
 
