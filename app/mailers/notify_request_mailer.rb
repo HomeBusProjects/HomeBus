48a8e3906c6b3dad428265646476f02980a5b47d
John Romkey <58883+romkey@users.noreply.github.com>
2020-10-10 18:21:08 -0700

    fix site_admn

--- app/mailers/notify_request_mailer.rb
+++ app/mailers/notify_request_mailer.rb
@@ -10,6 +10,6 @@ class NotifyRequestMailer < ApplicationMailer
   def admins_new_user
     @user = params[:user]
     @url  = 'http://example.com/login'
-    mail(to: User.where(site_admn: true).pluck(:email), subject: 'New user')
+    mail(to: User.where(site_admin: true).pluck(:email), subject: 'New user')
   end
 end
