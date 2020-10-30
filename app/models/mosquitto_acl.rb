60b55a5ce50cfb7d4385827a6ec1c2350eec136d
John Romkey <58883+romkey@users.noreply.github.com>
2020-10-29 22:24:22 -0700

    try to get the code to permit consuming DDCs to work

--- app/models/mosquitto_acl.rb
+++ app/models/mosquitto_acl.rb
@@ -20,17 +20,30 @@ class MosquittoAcl < MosquittoRecord
                                 provision_request_id: pr.id
           end
 
+          # for each consumable DDC we need to find all devices on the network which publish
+          # that DDC and create an entry permitting us to read and subscribe to them
           if ddc_device.consumable
-            # must iterate through all publishers
-            ddc_device.ddc.devices do |device|
-              MosquittoAcl.create username: account.id,
-                                  topic: "homebus/device/#{d.id}/#{ddc_device.ddc.name}",
-                                  permissions: 1+4,
-                                  provision_request_id: pr.id
+            ddc = ddc_device.ddc
+
+            pr.network.devices.each do |d|
+              names = DdcsDevice.joins(:ddc).where(device: d, consumable: true).pluck(:'ddcs.name')
+              if names.include? ddc_device.ddc.name
+                MosquittoAcl.create username: account.id,
+                                    topic: "homebus/device/#{d.id}/#{ddc_device.ddc.name}",
+                                    permissions: 1+4,
+                                    provision_request_id: pr.id
+              end
             end
           end
         end
       end
     end
   end
+
+  def self.from_device(device)
+    device_publishes = device.ddcs_devices.where(publishable: true).join(:ddcs).pluck(:'ddc.name')
+  end
+
+  private
+  
 end
