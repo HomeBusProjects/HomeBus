c53a5f4cef433d7509b6b0dc5a15a8837879ef38
John Romkey <58883+romkey@users.noreply.github.com>
2020-12-04 15:17:34 -0800

    temporarily make accounts no longer registerable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+#  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable
 
