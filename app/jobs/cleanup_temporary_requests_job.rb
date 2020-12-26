ac27dcc4f3e8e122723ff894e06ea14f656555cc
John Romkey <58883+romkey@users.noreply.github.com>
2020-12-26 14:48:11 -0800

    destroy temporary provision requests rather than delete

--- app/jobs/cleanup_temporary_requests_job.rb
+++ app/jobs/cleanup_temporary_requests_job.rb
@@ -2,6 +2,6 @@ class CleanupTemporaryRequestsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    ProvisionRequest.where('autoremove_at < ?', Time.now).delete_all
+    ProvisionRequest.where('autoremove_at < ?', Time.now).destroy_all
   end
 end
