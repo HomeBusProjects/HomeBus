7a47e4f7bc64bf0244a2f3d8d5ec0174ddc79d3e
John Romkey <58883+romkey@users.noreply.github.com>
2021-01-16 10:27:57 -0800

    added a little debugging for creating provision requests

--- app/controllers/provision_requests_controller.rb
+++ app/controllers/provision_requests_controller.rb
@@ -118,6 +118,9 @@ class ProvisionRequestsController < ApplicationController
         format.html { redirect_to @provision_request }
         format.json { render :show, status: :created, location: @provision_request }
       else
+        Rails.logger.error "ProvisionRequest.create failed"
+        Rails.logger.error @provision_request.errors
+
         format.html { render :new }
         format.json { render json: @provision_request.errors, status: :unprocessable_entity }
       end
