364eeb88bd352292c2107ffb1305dbd7fe06cace
John Romkey <58883+romkey@users.noreply.github.com>
2021-01-16 17:46:23 -0800

    added MosquittoAcl.from_provision_request_opposite which adds negation Acls and permits wildcard - experiment

--- app/models/mosquitto_acl.rb
+++ app/models/mosquitto_acl.rb
@@ -51,6 +51,58 @@ class MosquittoAcl < MosquittoRecord
     end
   end
 
+
+  def self.from_provision_request_opposite(pr)
+    account = MosquittoAccount.find_by provision_request_id: pr.id
+    puts "NEW ACCOUNT ID #{account.id}"
+
+    records = []
+
+    pr.devices.each do |device|
+      device.ddcs.each do |ddc|
+        Rails.logger.debug "PUBLISHES PERMISSION CHECK device ID: #{device.id}, network ID: #{pr.network.id}, ddc ID: #{ddc.id}"
+
+        if Permission.find_by(device: device, network: pr.network, ddc: ddc, publishes: false)
+          Rails.logger.debug "WRITE ACL homebus/device/#{device.id}/#{ddc.name}"
+
+            records.push MosquittoAcl.new(username: account.id,
+                                          topic: "homebus/device/#{device.id}/#{ddc.name}",
+                                          permissions: 0,
+                                          provision_request_id: pr.id)
+          end
+
+        # for each consumable DDC we need to find all devices on the network which publish
+        # that DDC and create an entry permitting us to read and subscribe to them
+        puts "CONSUMES PERMISSION CHECK device ID: #{device.id}, network ID: #{pr.network.id}, ddc ID: #{ddc.id}"
+
+        if Permission.find_by(device: device, network: pr.network, ddc: ddc, consumes: true)
+          Permission.where(network: pr.network, ddc: ddc, publishes: false).each do |p|
+            Rails.logger.debug "READ ACL homebus/device/#{p.device.id}/#{ddc.name}"
+            records.push MosquittoAcl.new(username: account.id,
+                                          topic: "homebus/device/#{p.device.id}/#{ddc.name}",
+                                          permissions: 0,
+                                          provision_request_id: pr.id)
+
+          end
+        end
+
+        records.push MosquittoAcl.new(username: account.id,
+                                      topic: "homebus/device/+/#{ddc.name}",
+                                      permissions: 4 + 1,
+                                      provision_request_id: pr.id)
+      end
+    end
+
+    Rails.logger.debug "#{records.length} records"
+    Rails.logger.debug "#{records.inspect}"
+
+    ActiveRecord::Base.transaction do
+      pr.mosquitto_acl.delete_all
+      records.each { |record| Rails.logger.debug "RECORD #{record.inspect}" ; record.save ; Rails.logger.debug record.errors.inspect }
+    end
+  end
+
+
   def self.from_device(device)
     device_publishes = device.ddcs_devices.where(publishable: true).join(:ddcs).pluck(:'ddc.name')
   end
