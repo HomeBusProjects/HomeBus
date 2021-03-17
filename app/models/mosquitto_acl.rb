02b26cba29851484d2f759da3c1fc16f2642f018
John Romkey <58883+romkey@users.noreply.github.com>
2021-03-16 22:29:01 -0700

    quote UUIDs in INSERT

--- app/models/mosquitto_acl.rb
+++ app/models/mosquitto_acl.rb
@@ -152,7 +152,7 @@ end
     device.ddcs.each do |ddc|
       Rails.logger.debug "ACL homebus/device/#{device.id}/#{ddc.name} -> #{permissions}"
 
-      records.push "INSERT INTO \"mosquitto_acls\" (\"username\", \"topic\", \"provision_request_id\", \"permissions\", \"created_at\", \"updated_at\") VALUES (#{account.id}, \"homebus/device/#{device.id}/#{ddc.name}\", #{pr.id}, #{permissions}, \"2021-03-16 03:52:12.630393\", \"2021-03-16 03:52:12.630393\");"
+      records.push "INSERT INTO \"mosquitto_acls\" (\"username\", \"topic\", \"provision_request_id\", \"permissions\", \"created_at\", \"updated_at\") VALUES (\"#{account.id}\", \"homebus/device/#{device.id}/#{ddc.name}\", \"#{pr.id}\", #{permissions}, \"2021-03-16 03:52:12.630393\", \"2021-03-16 03:52:12.630393\");"
     end
 
     records
