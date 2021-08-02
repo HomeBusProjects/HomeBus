e6989f4c815329b0cc267c335202e5ddba3a9299
John Romkey <58883+romkey@users.noreply.github.com>
2021-08-02 12:30:06 -0700

    updated auth job to work with new system and to not error out in development environments

--- app/jobs/update_mqtt_auth_job.rb
+++ app/jobs/update_mqtt_auth_job.rb
@@ -6,33 +6,36 @@ class UpdateMqttAuthJob < ApplicationJob
   queue_as :default
 
   def perform(pr)
-    command = "PGPASSWORD='#{ENV['MOSQUITTO_POSTGRESQL_PASSWORD']}' psql -h localhost -p 5432 -U #{ENV['MOSQUITTO_POSTGRESQL_USERNAME']} -d #{ENV['MOSQUITTO_POSTGRESQL_DATABASE']}"
+    command = "PGPASSWORD='#{pr.network.broker.postgresql_password}' psql -h localhost -p 5432 -U #{pr.network.broker.postgresql_username} -d #{pr.network.broker.postgresql_database}"
 
-    sql_commands = <<~END_OF_ACCOUNT
-      INSERT INTO "mosquitto_accounts" ("id", "password", "provision_request_id", "superuser", "enabled", "created_at", "updated_at") VALUES ('#{pr.account_id}', '#{pr.account_encrypted_password}', '#{pr.id}', false, true, NOW(), NOW()) ON CONFLICT ("id") DO UPDATE SET "password" = '#{pr.account_encrypted_password}', "updated_at" = NOW();
-    END_OF_ACCOUNT
+#    sql_commands = <<~END_OF_ACCOUNT
+#      INSERT INTO "mosquitto_accounts" ("id", "password", "provision_request_id", "superuser", "enabled", "created_at", "updated_at") VALUES ('#{pr.account_id}', '#{pr.account_encrypted_password}', '#{pr.id}', false, true, NOW(), NOW()) ON CONFLICT ("id") DO UPDATE SET "password" = '#{pr.account_encrypted_password}', "updated_at" = NOW();
+#    END_OF_ACCOUNT
 
-    sql_commands += MosquittoAcl.from_provision_request2(pr)
+    sql_commands = pr.broker_account.to_sql
+    sql_commands += BrokerAcl.from_provision_request2(pr)
 
     puts sql_commands
     puts command
 
     result = ''
 
-    Net::SSH.start(ENV['AUTOSSH_HOST'], ENV['AUTOSSH_USER'], key_data: ENV['AUTOSSH_PRIVATE_KEY']) do |session|
-      session.open_channel do |channel|
-        channel.exec(command) do |ch, success|
-          raise 'could not execute command' unless success
+    unless Rails.env.development?
+      Net::SSH.start(broker.ssh_hostname, broker.ssh_username, key_data: broker.ssh_key) do |session|
+        session.open_channel do |channel|
+          channel.exec(command) do |ch, success|
+            raise 'could not execute command' unless success
 
-          ch.send_data sql_commands
-          ch.eof!
+            ch.send_data sql_commands
+            ch.eof!
 
-          ch.on_data { |_c, data| print data }
-          ch.on_extended_data { |_c, _type, data| print data }
+            ch.on_data { |_c, data| print data }
+            ch.on_extended_data { |_c, _type, data| print data }
+          end
         end
-      end
 
-      session.loop
+        session.loop
+      end
     end
 
     pr.ready = true
