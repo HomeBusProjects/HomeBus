64cff32b684a9bcf04a677102f437b0294611f67
John Romkey <58883+romkey@users.noreply.github.com>
2021-08-15 18:00:03 -0700

    temporary request cleanup should use NetworkMonitor not Monitor

--- app/jobs/cleanup_temporary_requests_job.rb
+++ app/jobs/cleanup_temporary_requests_job.rb
@@ -4,6 +4,6 @@ class CleanupTemporaryRequestsJob < ApplicationJob
   queue_as :default
 
   def perform(*_args)
-    Monitor.where('last_accessed < ?', Time.zone.now - 10.minutes).destroy_all
+    NetworkMonitor.where('last_accessed < ?', Time.zone.now - 10.minutes).destroy_all
   end
 end
