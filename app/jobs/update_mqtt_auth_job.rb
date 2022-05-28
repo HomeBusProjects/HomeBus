6ae99c806fd29779cbd58a326297ba43d48851ca
John Romkey <58883+romkey@users.noreply.github.com>
2022-05-27 20:01:42 -0700

    debug mqtt auth update

--- app/jobs/update_mqtt_auth_job.rb
+++ app/jobs/update_mqtt_auth_job.rb
@@ -7,10 +7,11 @@ class UpdateMqttAuthJob < UpdateRemoteBrokerJob
 
   def perform(pr)
     sql_commands = pr.broker_account.to_sql
-    sql_commands += BrokerAcl.from_provision_request(pr)
+#    sql_commands += BrokerAcl.from_provision_request(pr)
 
     Rails.logger.error '>>>> MQTT AUTH <<<<'
-    Rails.logger.error sql_commands[0, 20] + '...' + sql_commands[-10, 10]
+    #    Rails.logger.error sql_commands[0, 20] + '...' + sql_commands[-10, 10]
+    Rails.logger.error sql_commands
 
     update_remote_broker(pr.network.broker, sql_commands)
 
