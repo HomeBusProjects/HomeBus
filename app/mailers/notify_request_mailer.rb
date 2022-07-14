bb9ef4ffc9673935b8ed50d234cd6b7987d8cbda
John Romkey <58883+romkey@users.noreply.github.com>
2022-07-13 17:34:10 -0700

    updated gems
    work on mailers

--- app/mailers/notify_request_mailer.rb
+++ app/mailers/notify_request_mailer.rb
@@ -11,10 +11,24 @@ class NotifyRequestMailer < ApplicationMailer
 
   def admins_new_user
     @user = params[:user]
-    @url  = 'http://example.com/login'
+    @url  = 'http://homebus.org/login'
     if User.where(site_admin: true).count.positive?
       mail(to: User.where(site_admin: true).pluck(:email),
            subject: 'New user')
     end
+  end
+
+  def new_device
+    @device = params[:device]
+    @network = params[:network]
+    @user = params[:user]
+    mail(to: @user.email, subject: "Device #{@device.friendly_name} has joined your network #{@network.name}")
+  end
+
+  def removed_device
+    @device = params[:device]
+    @network = params[:network]
+    @user = params[:user]
+    mail(to: @user.email, subject: "Device #{@device.friendly_name} has left your network #{@network.name}")
   end
 end
