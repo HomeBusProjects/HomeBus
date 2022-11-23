32c1e816f7fb6fd219c7321625dd24e450d37b5c
John Romkey <58883+romkey@users.noreply.github.com>
2022-11-23 12:18:26 -0800

    updated gems
    don't output debugging info in ACL job

--- app/jobs/update_mqtt_acl_job.rb
+++ app/jobs/update_mqtt_acl_job.rb
@@ -6,8 +6,8 @@ class UpdateMqttAclJob < UpdateRemoteBrokerJob
   def perform(pr)
     sql_commands = BrokerAcl.from_provision_request(pr)
 
-    Rails.logger.error '>>> MQTT ACL <<<<'
-    Rails.logger.error sql_commands[0, 20] + '...' + sql_commands[-10, 10]
+#    Rails.logger.error '>>> MQTT ACL <<<<'
+#    Rails.logger.error sql_commands[0, 20] + '...' + sql_commands[-10, 10]
 
     update_remote_broker(pr.network.broker, sql_commands)
 
